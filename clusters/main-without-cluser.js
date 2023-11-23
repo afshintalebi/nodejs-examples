67892389689dccf9c228126c0f18e6466f4a0466
Afshin <afshin.talebi@gmail.com>
2023-11-23 13:29:30 +0330

    add clustering example

---
+++ clusters/main-without-cluser.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/:n", function (req, res) {
+  let n = parseInt(req.params.n);
+  let count = 0;
+
+  if (n > 5000000000) n = 5000000000;
+
+  for (let i = 0; i <= n; i++) {
+    count += i;
+  }
+
+  res.send(`Final count is ${count}`);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
